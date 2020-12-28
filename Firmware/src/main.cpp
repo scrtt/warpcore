302fd2fa9284f9d9636f594c2ad32a11ca8a3ddd
scrtt <chilledtimmy@googlemail.com>
2020-12-28 01:19:03 +0100

    include wifi cerdentials

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#include "wifi.h"
 
 void setup() {
   // put your setup code here, to run once:
---
+++ Firmware/src/wifi.h
@@ -0,0 +1,7 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+const char* ssid = "SSID";
+const char* password = "key";
+
+#endif

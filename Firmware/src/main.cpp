d0b4df838b931ebc994ab069c6ed661343c3c5ce
scrtt <chilledtimmy@googlemail.com>
2020-12-28 14:16:39 +0100

    added hello world webserver

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 
 #include "wifi.h"
+#include <FS.h> 
+#include "ESPAsyncWebServer.h"
+FS *filesystem = &SPIFFS;
 
-void setup() {
-  Serial.begin(9600);
 
-  // Connect to Wifi
+AsyncWebServer server(80);
+
+
+void setup()
+{
+    Serial.begin(9600);
+    filesystem->begin();
+  
+  
+
+  // connect to wifi
   Serial.println("Connecting to ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print(".");
   }
@@ -26,8 +36,16 @@ void setup() {
   WiFi.macAddress(macAddr);
   Serial.printf("mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 
+server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false);
+  });
+
+  server.begin();
+ 
+ }
+
+void loop()
+{
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}

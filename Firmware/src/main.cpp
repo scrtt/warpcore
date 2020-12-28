f404132189d475173d30dd08c1c8d541215af3b2
scrtt <chilledtimmy@googlemail.com>
2020-12-28 01:53:41 +0100

    wifi connection

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -10,7 +10,22 @@ void setup() {
   // Connect to Wifi
   Serial.println("Connecting to ");
   Serial.println(ssid);
+
   WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("my IP: ");
+  Serial.println(WiFi.localIP());
+  uint8_t macAddr[6];
+  WiFi.macAddress(macAddr);
+  Serial.printf("mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+
 }
 
 void loop() {

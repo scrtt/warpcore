5b9263fb6775d07ad47fce2fdcdf535cbf636f55
scrtt <chilledtimmy@googlemail.com>
2021-01-01 00:35:57 +0100

    non working neopixel test

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,17 +1,26 @@
 #include <Arduino.h>
+
 #include <ESP8266WiFi.h>
 
 #include "wifi.h"
 #include <FS.h>
 #include "ESPAsyncWebServer.h"
 
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 4
+#define NUM_LEDS 5
+#define COLOR_ORDER GRB
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN);
+
 AsyncWebServer server(80);
 
-  String processor(const String& var)
+String processor(const String &var)
+{
+  if (var == "HEADLINE")
   {
-  if(var == "HEADLINE")
-  {
-    String headline ="<h1>Hallo</h1>";
+    String headline = "<h1>Hallo</h1>";
     return headline;
   }
   return String();
@@ -19,6 +28,11 @@ AsyncWebServer server(80);
 
 void setup()
 {
+  //init leds
+  pixels.begin();
+  pixels.setPixelColor(1, pixels.Color(255, 255, 255));
+  pixels.show();
+
   Serial.begin(9600);
   SPIFFS.begin();
 
@@ -41,8 +55,6 @@ void setup()
   WiFi.macAddress(macAddr);
   Serial.printf("mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 
-
-
   //starting webserver
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/index.html", String(), false, processor);
@@ -53,5 +65,4 @@ void setup()
 
 void loop()
 {
-
 }

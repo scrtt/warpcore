2313653aa5613e9a533d4be2c6182ad127e76994
Unim4trixZero <daro.wob@gmail.com>
2021-01-14 02:56:07 +0100

    "bool a" entfernt, wird nicht mehr benötigt 3 led array's erstellt

--- Firmware/src/ledanimations.h
+++ Firmware/src/ledanimations.h
@@ -5,14 +5,18 @@
 class ledanimations final
 {
 private:
-    static constexpr size_t num_leds{37U};
-    CRGB leds[num_leds];
+    static constexpr size_t NUM_LEDS{37U};
+
+    CRGBArray<NUM_LEDS> leds;
+    CRGBSet leds1;
+    CRGBSet leds2;
+    CRGBSet leds3;
+    uint8_t gHue;
+
     int activeAnimation;
     void warpcore();
     void animation_1();
     void animation_2();
-    uint8_t gHue;
-    bool a = false;
 
 public:
     ledanimations();
